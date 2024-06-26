24d58cb5bfbb60b634d69f90ae20b4da730c2429
FranciscoCosta <francisco100eg@gmail.com>
2024-06-25 22:13:07 -0300

    chore(web3) : added web3 as a depedencie

--- src/lib/Web3Service.ts
+++ src/lib/Web3Service.ts
@@ -2,9 +2,11 @@ import { toast } from 'react-toastify';
 
 export async function mint() {
 
-    if((window as any).ethereum) {
+    if((!window as any).ethereum) {
         toast.error('Please install Metamask to mint ProtoCoins');
         return;
+    }else{
+        toast.success('Metamask installed');    
     }
         
 }
